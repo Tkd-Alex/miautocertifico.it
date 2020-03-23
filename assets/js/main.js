62f629b538d7b073cc182e11e73df9580286be08
Alessandro Maggio <alex.tkd.alex@gmail.com>
2020-03-23 23:41:30 +0100

    Form input-name, big button for destination pdf, FAQ

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,85 @@ undoButton.addEventListener("click", function (event) {
         signaturePad.fromData(data);
     }
 });
+
+// Opening animation
+
+$.Velocity.RegisterEffect('fadeInPanels', {
+    defaultDuration: 500,
+    calls: [
+        [{
+            opacity: [1, 0.3],
+            scale: [1, 0.5]
+        }]
+    ]
+});
+
+$('.panel').velocity('fadeInPanels', {
+    stagger: 250,
+})
+
+// Accordions
+
+$('.accordion-link').click(function (e) {
+    e.preventDefault();
+
+    var accordionWrapper = $(this).closest('.accordion'),
+        accordionActivePanel = $(this).closest('.panel'),
+        accordionActiveLink = accordionWrapper.find('.active');
+
+    if ($(this).closest('.panel').find('.content-collapse').hasClass('velocity-animating')) {
+        return false;
+    } else if (e.handled !== true) {
+        accordionActiveLink.closest('.panel').find('.content-collapse').attr('aria-expanded', false).velocity('slideUp', {
+            easing: 'easeOutQuad'
+        });
+        if ($(this).hasClass('active')) {
+            $(this).attr('aria-expanded', false).removeClass('active');
+        } else {
+            accordionActivePanel.find('.content-collapse').attr('aria-expanded', true).velocity('slideDown', {
+                easing: 'easeOutQuad'
+            });
+            accordionWrapper.find('.accordion-link').removeClass('active').attr('aria-expanded', false);
+            $(this).attr('aria-expanded', true).addClass('active');
+        }
+        e.handled = true;
+    } else {
+        return false;
+    }
+});
+
+// Toggles
+
+$('.toggle-link').click(function (e) {
+    e.preventDefault();
+    var toggleActivePanel = $(this).closest('.panel');
+
+    if (toggleActivePanel.find('.content-collapse').hasClass('velocity-animating')) {
+        return false;
+    } else if (e.handled !== true) {
+        if ($(this).hasClass('active')) {
+            toggleActivePanel.find('.content-collapse').attr('aria-expanded', false).velocity('slideUp', {
+                easing: 'easeOutQuad'
+            });
+            $(this).attr('aria-expanded', false).removeClass('active');
+        } else {
+            toggleActivePanel.find('.content-collapse').attr('aria-expanded', true).velocity('slideDown', {
+                easing: 'easeOutQuad'
+            });
+            $(this).attr('aria-expanded', true).addClass('active');
+        }
+        e.handled = true;
+    } else {
+        return false;
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
